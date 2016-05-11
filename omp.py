ca7957ab7ada6a6b2813505f95bc48667ee3dd84
Geoffrey Chisnall <arcainus@gmail.com>
2016-05-11 08:32:33 +0200

    Added some more menu items and reworked some others

--- omp.py
+++ omp.py
@@ -45,13 +45,17 @@ while ans:
     5.List the Portlist
     6.Create a new TARGET
     7.Create a new TASK
-    8.Delete a TASK
-    9.Delete a TARGET
-    10.Start a TASK
-    11.Stop a TASK
-    12.Get latest report of TASK
-    13.Get latest report of all TASKS
-    14.List status of a TASK
+    8.Create a new PORTLIST
+    9.Delete a TASK
+    10.Delete a TARGET
+    11.Delete a PORTLIST
+    12.Start a TASK
+    13.Stop a TASK
+    14.Start all TASKS (NOTE: this can effect performance on the computer)
+    15.Get latest report of TASK
+    16.Get latest report of all TASKS
+    17.List status of a TASK
+    18.Modify TARGET's PORTLIST (NOTE: the target has to be removed from a TASK)
     0.Exit/Quit
     """)
     ans=input("What would you like to do? ")
@@ -116,32 +120,75 @@ while ans:
      idtarget=raw_input("ID of the TARGET to use: ")
      os.system("omp -u %s -w %s --xml='<create_task><name>%s</name><comment>%s</comment><config id=\"%s\"/><target id=\"%s\"/></create_task>'" % (username,password,nametask,commenttask,configtask,idtarget))
 
-#Delete a task
+#Create a new Portlist
     elif ans==8:
+     nameportlist=raw_input("Name of the Portlist: ")
+     commentportlist=raw_input("Comment of the Portlist: ")
+     print ("Usage: T:1-65535 or T:22,25,8080 or T:1-100,U:1-100 - T=TCP;U=UDP")
+     rangeportlist=raw_input("Ports to add: ")
+     os.system("omp -u %s -w %s --xml='<create_port_list><name>%s</name><comment>%s</comment><port_range>%s</port_range></create_port_list>'" % (username,password,nameportlist,commentportlist,rangeportlist))
+
+#Delete a task
+    elif ans==9:
      os.system("omp -u %s -w %s -G" % (username,password))
      idtask=raw_input("ID of the TASK: ")
      os.system("omp -u %s -w %s -D %s" % (username,password,idtask))
 
 #Delete a target
-    elif ans==9:
+    elif ans==10:
      os.system("omp -u %s -w %s -T" % (username,password))
      idtarget=raw_input("ID of the TARGET: ")
      os.system("omp -u %s -w %s --xml='<delete_target target_id=\"%s\" />'" % (username,password,idtarget))
 
+#Delete a portlist
+    elif ans==11:
+      os.system("echo \"<xml>\" > portlist.xml")
+      os.system("omp -u %s -w %s --xml='<get_port_lists/>' >> portlist.xml" % (username,password))
+      os.system("echo \"</xml>\" >> portlist.xml")
+      xmldoc = minidom.parse("portlist.xml")
+      xml = xmldoc.getElementsByTagName("xml")[0]
+      ports = xml.getElementsByTagName("port_list")
+      print "PORTID", ' '*30, "PORTAME"
+      for portlist in ports:
+        portname = portlist.getElementsByTagName("name")[1].firstChild.data
+        portid = portlist.getAttribute("id")
+        print portid, portname
+      deleteportlist=raw_input("Portlist to delete: ")
+      os.system("omp -u %s -w %s --xml='<delete_port_list port_list_id=\"%s\" />>'" % (username,password,deleteportlist))
+     
+
 #Start a task
-    elif ans==10:
+    elif ans==12:
       os.system("omp -u %s -w %s -G" % (username,password))
       taskid=raw_input("ID of the TASK: ").strip()
       os.system("omp -u %s -w %s -S %s" % (username,password,taskid))
 
 #Stop a task
-    elif ans==11:
+    elif ans==13:
      os.system("omp -u %s -w %s -G" % (username,password))
      taskid=raw_input("ID of the TASK: ").strip()
      os.system("omp -u %s -w %s -iX '<stop_task task_id=\"%s\"/>'" % (username, password, taskid))
 
+#Start all tasks
+    elif ans==14:
+        os.system("echo \"<xml>\" > tasks.xml")
+        os.system("omp -u %s -w %s --xml='<get_tasks/>' >> tasks.xml" % (username,password))
+        os.system("echo \"</xml>\" >> tasks.xml")
+
+        xmldoc = minidom.parse("tasks.xml")
+        xml = xmldoc.getElementsByTagName("xml")[0]
+        tasks = xml.getElementsByTagName("task")
+
+        f = open('tasks.xml', 'rb')
+        f.readlines()
+        f.close()
+        for task in tasks:
+                taskid = task.getAttribute("id")
+ 		os.system("omp -u %s -w %s -S %s" % (username,password,taskid))
+        os.system("rm tasks.xml")
+
 #get the lastest report on a task
-    elif ans==12: 
+    elif ans==15: 
      os.system("omp -u %s -w %s -G" % (username,password))
      taskid=raw_input("ID of TASK: ")
      taskname=("omp -u %s -w %s -iX '<get_tasks task_id=\"%s\" />' | sed -n '/<name/,/name>/p' | grep \"<name>\"  | sed -e 's/.<name>//' | sed -e 's/<.*//' | awk 'NR==2' | sed \"s/       //\"" %  (username,password,taskid))
@@ -153,7 +200,7 @@ while ans:
      os.system("omp -u %s -w %s --get-report %s --format c402cc3e-b531-11e1-9163-406186ea4fc5 > \"%s\".pdf" % (username,password,result2,resultname2))
 
 #get the latest report on all tasks
-    elif ans==13:
+    elif ans==16:
 	os.system("echo \"<xml>\" > tasks.xml")
 	os.system("omp -u %s -w %s --xml='<get_tasks/>' >> tasks.xml" % (username,password))
 	os.system("echo \"</xml>\" >> tasks.xml")
@@ -166,26 +213,51 @@ while ans:
 	f.readlines()
 	f.close()
 	for task in tasks:
-        	taskid = task.getAttribute("id")
-	        taskname=("omp -u %s -w %s -iX '<get_tasks task_id=\"%s\" />' | sed -n '/<name/,/name>/p' | grep \"<name>\"  | sed -e 's/.<name>//' | sed -e 's/<.*//' | awk 'NR==2' | sed \"s/       //\"" %  (username,password,taskid))
-        	resultname = subprocess.check_output(taskname, shell=True)
+      	 	taskid = task.getAttribute("id")
+		taskstatus=("omp -u %s -w %s -iX '<get_tasks task_id=\"%s\" />' | grep \"<status>\" | sed -e 's/.<status>//' | sed -e 's/<.*//'" %  (username,password,taskid))
+		status = subprocess.check_output(taskstatus, shell=True)
+		taskname=("omp -u %s -w %s -iX '<get_tasks task_id=\"%s\" />' | sed -n '/<name/,/name>/p' | grep \"<name>\"  | sed -e 's/.<name>//' | sed -e 's/<.*//' | awk 'NR==2' | sed \"s/       //\"" %  (username,password,taskid))
+     		resultname = subprocess.check_output(taskname, shell=True)
 	        resultname2 = resultname.strip()
-	        taskreport="omp -u %s -w %s -iX '<get_tasks task_id=\"%s\" />' | sed -n '/<last_report/,/last_report>/p' | grep \"report id\"  | sed -e 's/<report id=\"//' | sed -e 's/\">.*//' | sed \"s/          //\"" % (username,password,taskid)
-        	result = subprocess.check_output(taskreport, shell=True)
-	        result2 = result.strip()
-	        os.system("omp -u %s -w %s --get-report %s --format c402cc3e-b531-11e1-9163-406186ea4fc5 > \"%s\".pdf" % (username,password,result2,resultname2))
-	        print ("%s report has been generated" % (resultname2))
+		if "New" in status:
+			print ("%s report has not been generated" % (resultname2))
+		else:
+		        taskreport="omp -u %s -w %s -iX '<get_tasks task_id=\"%s\" />' | sed -n '/<last_report/,/last_report>/p' | grep \"report id\"  | sed -e 's/<report id=\"//' | sed -e 's/\">.*//' | sed \"s/          //\"" % (username,password,taskid)
+        		result = subprocess.check_output(taskreport, shell=True)
+	        	result2 = result.strip()
+		        os.system("omp -u %s -w %s --get-report %s --format c402cc3e-b531-11e1-9163-406186ea4fc5 > \"%s\".pdf" % (username,password,result2,resultname2))
+		        print ("%s report has been generated" % (resultname2))
 	os.system("rm tasks.xml")
 
 
 #Get the status on a task
-    elif ans==14:
+    elif ans==17:
      os.system("omp -u %s -w %s -G" % (username,password))
      taskid=raw_input("ID of TASK: ")
      taskstatus="omp -u %s -w %s -G | grep %s | awk '{print $2}'" % (username,password,taskid)
      result = subprocess.check_output(taskstatus, shell=True)
      result2 = result.strip()
      print "The status of task %s is %s" % (taskid,result)
+
+#Modify TARGETS's PORTLIST
+    elif ans==18:
+	os.system("omp -u %s -w %s -T" % (username,password))
+	modifytarget=raw_input("ID of TARGET: ")
+	os.system("echo \"<xml>\" > portlist.xml")
+	os.system("omp -u %s -w %s --xml='<get_port_lists/>' >> portlist.xml" % (username,password))
+	os.system("echo \"</xml>\" >> portlist.xml")
+	xmldoc = minidom.parse("portlist.xml")
+	xml = xmldoc.getElementsByTagName("xml")[0]
+	ports = xml.getElementsByTagName("port_list")
+	print "PORTID", ' '*30, "PORTAME"
+	for portlist in ports:
+		portname = portlist.getElementsByTagName("name")[1].firstChild.data
+		portid = portlist.getAttribute("id")
+		print portid, portname
+	modifytargetportlist=raw_input("ID of PORTLIST: ")
+	os.system("omp -u %s -w %s --xml='<modify_target target_id=\"%s\"><port_list>%s</port_list></modify_target>'" % (username,password,modifytarget,modifytargetportlist))
+     
+     
 
 #Exit the menu
     elif ans==0:
